bd0b38abaa27b9bd72f2f3547ae8b25737fb4c94
vitorsei <seiji.vitor@gmail.com>
2015-09-09 21:29:50 -0300

    Adding not added files previously

---
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+eventsApp.controller('EditEventController',
+    function EditEventController($scope, eventData) {
+
+        $scope.saveEvent = function (event, newEventForm) {
+            if (newEventForm.$valid) {
+                eventData.save(event)
+                    .$promise
+                    .then(function(response) { console.log('success', response)} )
+                    .catch(function(response) { console.log('failure', response)} );
+            }
+        };
+
+        $scope.cancelEdit = function () {
+            window.location = 'EventDetails.html';
+        };
+    }
+);
