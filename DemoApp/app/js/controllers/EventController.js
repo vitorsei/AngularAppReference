c1c0bfc33246f81b6a2e6f81a9282150dc77a6bd
vitorsei <seiji.vitor@gmail.com>
2015-09-13 19:04:12 -0300

    Adding AngularJS Routing Feature

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -1,67 +1,87 @@
 ﻿'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData, $anchorScroll, $cookieStore) {
+    function EventController($scope, eventData) {
 
         $scope.sortOrder = 'name';
-        
-        //---------------------Uses $http service----------------------
-        //eventData.getEvent()
-        //    .success(function (event) { $scope.event = event; })
-        //    .error(function (data, status, heardes, config) {
-        //        $log.warn(data, status, heardes, config);
-        //    });
 
-        //---------------------Uses $resource service----------------------
-        //$scope.event = eventData.getEvent();
         eventData.getEvent()
             .$promise
             .then(function (event) { $scope.event = event;})
             .catch(function (response) { console.log(response); })
 
         $scope.upVoteSession = function (session) {
-            var sessionsStored = $cookieStore.get('vote');
-
-            if (typeof sessionsStored === 'undefined') {
-                $cookieStore.put('vote', [session.id]);
-            }
-            else {
-                if (sessionsStored.indexOf(session.id) == -1) {
-                    sessionsStored.push(session.id);
-                    $cookieStore.put('vote', sessionsStored);
-                }
-                else {
-                    alert('You have already voted');
-                    return;
-                }
-            }
-
             session.upVoteCount++;
         };
 
         $scope.downVoteSession = function (session) {
-            var sessionsStored = $cookieStore.get('vote');
-
-            if (typeof sessionsStored === 'undefined') {
-                $cookieStore.put('vote', [session.id]);
-            }
-            else {
-                if (sessionsStored.indexOf(session.id) == -1) {
-                    sessionsStored.push(session.id);
-                    $cookieStore.put('vote', sessionsStored);
-                }
-                else {
-                    alert('You have already voted');
-                    return;
-                }
-            }
-
             session.upVoteCount--;
         };
-
-        $scope.scrollToSession = function () {
-            $anchorScroll();
-        }
-
     }
 );
+
+//eventsApp.controller('EventController',
+//    function EventController($scope, eventData, $anchorScroll, $cookieStore) {
+
+//        $scope.sortOrder = 'name';
+        
+//        //---------------------Uses $http service----------------------
+//        //eventData.getEvent()
+//        //    .success(function (event) { $scope.event = event; })
+//        //    .error(function (data, status, heardes, config) {
+//        //        $log.warn(data, status, heardes, config);
+//        //    });
+
+//        //---------------------Uses $resource service----------------------
+//        //$scope.event = eventData.getEvent();
+//        eventData.getEvent()
+//            .$promise
+//            .then(function (event) { $scope.event = event;})
+//            .catch(function (response) { console.log(response); })
+
+//        $scope.upVoteSession = function (session) {
+//            var sessionsStored = $cookieStore.get('vote');
+
+//            if (typeof sessionsStored === 'undefined') {
+//                $cookieStore.put('vote', [session.id]);
+//            }
+//            else {
+//                if (sessionsStored.indexOf(session.id) == -1) {
+//                    sessionsStored.push(session.id);
+//                    $cookieStore.put('vote', sessionsStored);
+//                }
+//                else {
+//                    alert('You have already voted');
+//                    return;
+//                }
+//            }
+
+//            session.upVoteCount++;
+//        };
+
+//        $scope.downVoteSession = function (session) {
+//            var sessionsStored = $cookieStore.get('vote');
+
+//            if (typeof sessionsStored === 'undefined') {
+//                $cookieStore.put('vote', [session.id]);
+//            }
+//            else {
+//                if (sessionsStored.indexOf(session.id) == -1) {
+//                    sessionsStored.push(session.id);
+//                    $cookieStore.put('vote', sessionsStored);
+//                }
+//                else {
+//                    alert('You have already voted');
+//                    return;
+//                }
+//            }
+
+//            session.upVoteCount--;
+//        };
+
+//        $scope.scrollToSession = function () {
+//            $anchorScroll();
+//        }
+
+//    }
+//);
