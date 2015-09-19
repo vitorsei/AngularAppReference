ab9bae89dd1c8d151bde24f8e3f8149fae5f6570
vitorsei <seiji.vitor@gmail.com>
2015-09-18 22:13:31 -0300

    Adding DemoApp2 Static WebSite

---
+++ DemoApp2/app/js/controllers/EventListController.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+eventsApp.controller('EventListController',
+    function EventListController($scope, $location, eventData) {
+        $scope.events = eventData.getAllEvents();
+    }
+);
---
+++ DemoApp2/app/js/services/EventData.js
@@ -0,0 +1,26 @@
+﻿
+//---------------------Uses $http service----------------------
+//eventsApp.factory('eventData', function ($http) {
+//    return {
+//        getEvent: function () {
+//            return $http({ method: 'GET', url: 'http://localhost:18774/Event/1' });
+//        }
+//    };
+//});
+
+//---------------------Uses $resource service----------------------
+eventsApp.factory('eventData', function ($resource) {
+    var resource = $resource('http://localhost:9000/Event/:id', { id: '@id' });
+
+    return {
+        getEvent: function (eventId) {
+            return resource.get({ id: eventId });
+        },
+        save: function (event) {
+            return resource.save(event);
+        },
+        getAllEvents: function () {
+            return resource.query();
+        }
+    };
+});
