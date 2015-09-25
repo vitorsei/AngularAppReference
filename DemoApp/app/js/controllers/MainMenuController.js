d568710c2b2b1f452df92c9a3703f27be6aee5ac
vitorsei <seiji.vitor@gmail.com>
2015-09-24 23:58:28 -0300

    Using Angular's $location Service

---
+++ DemoApp/app/js/controllers/MainMenuController.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+eventsApp.controller('MainMenuController',
+    function MainMenuController($scope, $location) {
+        $scope.createEvent = function () {
+
+            ////url for test http://localhost:8000/newEvent?foo=bar&eventId=2#qux
+            //console.log('absUrl: ', $location.absUrl());
+            //console.log('protocol: ', $location.protocol());
+            //console.log('port: ', $location.port());
+            //console.log('host: ', $location.host());
+            //console.log('path: ', $location.path());
+            //console.log('search: ', $location.search());
+            //console.log('hash: ', $location.hash());
+            //console.log('url: ', $location.url());
+
+
+            $location.url('/newEvent');
+        };
+    });
