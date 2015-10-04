d9b9384e769d8089f83211f46427cef35b851547
vitorsei <seiji.vitor@gmail.com>
2015-10-04 14:40:54 -0300

    Testing Filters

---
+++ DemoApp/test/unit/DurationsSpec.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('durations', function() {
+
+    beforeEach(module("eventsApp"));
+
+    it('should return "Half Hour" when given a 1', inject(function(durationsFilter){
+        expect(durationsFilter(1)).toEqual('Half Hour');
+    }))
+
+    it('should return "1 Hour" when given a 2', inject(function(durationsFilter){
+        expect(durationsFilter(2)).toEqual('1 Hour');
+    }))
+
+    it('should return "Half Day" when given a 3', inject(function(durationsFilter){
+        expect(durationsFilter(3)).toEqual('Half Day');
+    }))
+
+    it('should return "Full Day" when given a 4', inject(function(durationsFilter){
+        expect(durationsFilter(4)).toEqual('Full Day');
+    }))
+});
